6792518d9f98d8e914052a20243ac104f9585a09
Lizzy <lizzyd710@gmail.com>
2016-02-08 17:29:12 -0500

    Trying to split the upload process into two parts

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -6,15 +6,16 @@ class ChaptersController < ApplicationController
 		@chapter = Chapter.new		
 	end
 	def create
-		@chapter = Chapter.new(chapter_params)		
+		@chapter = Chapter.new(chapter_params)
+		@chapter.work_id= params[:work_id] #hopefully this should get the id from the url
 	end
 	def show
 		@chapter = Chapter.find(params[:id])
-		@work = @chapter.work		
+		@work = @chapter.work	
 	end
 
 private
 	def chapter_params
-		params.require(:chapter).permit(:title, :chapter_number, :work_id, :content)
+		params.require(:chapter).permit(:title, :chapter_number, :content)
 	end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -18,7 +18,8 @@ class WorksController < ApplicationController
 		@chapter.chapter_number = 1 #says that the chapter that was just created is the first chapter in this work
 		@work.chapter_id = @chapter #sets the chapter that was just created as the chapter which is opened when the work is first opened (aka the first chapter)
 		if @work.save
-			redirect_to '/home'
+			redirect_to newchapter_path(@work.id) #This should take the user to the next page in upload process where you input content. Not sure how to get the id part of the url to be
+			#the id of the work that was just created. Have to find out.
 		else
 			render 'new'
 		@chapter.work_id = @work.id #says the chapter that was created belongs to the work that was just created
@@ -33,6 +34,6 @@ class WorksController < ApplicationController
 	end
 private
 	def work_params
-		params.require(:work).permit(:title, :description, :fandom_id, :content)
+		params.require(:work).permit(:title, :description, :fandom_id)
 	end
 end
