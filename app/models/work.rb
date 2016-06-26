f521d5268df5ee269511fe6418264662395019c3
Lizzy <lizzyd710@gmail.com>
2016-06-26 15:31:15 -0400

    Fixed typo and added comment

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,8 +1,8 @@
 class Work < ActiveRecord::Base
-	belongs_to :fan
+	belongs_to :fandom
 	has_many :characters
 	belongs_to :user
-	has_many :chapters
+	has_many :chapters #the ao3 work model does have this so I did one thing right
 	
 	#self.abstract_class = true 
 	#will be an abstract class but won't be for testing
